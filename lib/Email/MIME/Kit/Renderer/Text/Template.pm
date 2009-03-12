32164c6b8bcd45b32c13b094f8c841fe15f91e3c
Ricardo SIGNES <rjbs@cpan.org>
2009-03-12 03:09:55 +0000

    Import of RJBS/Email-MIME-Kit-Renderer-Text-Template-1.000 from CPAN.
    gitpan-cpan-distribution: Email-MIME-Kit-Renderer-Text-Template
    gitpan-cpan-version:      1.000
    gitpan-cpan-path:         RJBS/Email-MIME-Kit-Renderer-Text-Template-1.000.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ lib/Email/MIME/Kit/Renderer/Text/Template.pm
@@ -0,0 +1,69 @@
+package Email::MIME::Kit::Renderer::Text::Template;
+our $VERSION = '1.000';
+
+use Moose;
+with 'Email::MIME::Kit::Role::Renderer';
+# ABSTRACT: render parts of your mail with Text::Template
+
+use Text::Template ();
+
+sub _enref_as_needed {
+  my ($self, $hash) = @_;
+
+  my %return;
+  while (my ($k, $v) = each %$hash) {
+    $return{ $k } = (ref $v and not blessed $v) ? $v : \$v;
+  }
+
+  return \%return;
+}
+
+has template_args => (
+  is  => 'ro',
+  isa => 'HashRef',
+);
+
+sub render  {
+  my ($self, $input_ref, $args)= @_;
+
+  my $hash = $self->_enref_as_needed({
+    (map {; $_ => ref $args->{$_} ? $args->{$_} : \$args->{$_} } keys %$args),
+  });
+
+  my $result = Text::Template::fill_in_string(
+    $$input_ref,
+    %{ $self->{template_args} || {} },
+    HASH => $hash,
+  );
+
+  return \$result;
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Email::MIME::Kit::Renderer::Text::Template - render parts of your mail with Text::Template
+
+=head1 VERSION
+
+version 1.000
+
+=head1 AUTHOR
+
+  Ricardo Signes <rjbs@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Ricardo Signes.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut 
+
+
